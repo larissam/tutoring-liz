df14e78695a6f390b37cc612107ee3f76758e21c
Larissa Muramoto <lmuramoto@palantir.com>
2018-08-19 19:38:17 -0700

    add 8.19 exercises

---
+++ lesson-8.19/menu.js
@@ -0,0 +1,7 @@
+// Start by creating an empty menu object.
+// Add a dishes property to your menu object and set its value to an empty array.
+// Create a method called "setDishes" that takes in a new array of dishes and sets the menu's dishes array to be the new array
+// Add a method called "addDish" that will add a new dish
+// The method should take in two parameters: the dishName and the dishPrice.
+// The method should create an object called dish which has a name and price which it gets from the parameters.
+// The method should then push this dish object into the menu's dishes array
---
+++ lesson-8.19/test.js
@@ -0,0 +1,123 @@
+const instructors = [{
+  name: 'Larissa',
+  interests: ['cats', 'psychology', 'weird food']
+}, {
+  name: 'Sasha',
+  interests: ['travel', 'food', 'accordion']
+}];
+
+const classroom = {
+  name: 'FEWD',
+  time: '6:30 PM, T/Th',
+  room: 3,
+  instructor: instructors[0],
+  students: [{
+    name: 'Liz',
+    interests: ['harry styles', 'mindfulness', 'dessert']
+  }, {
+    name: 'Jessica',
+    interests: ['philanthropy', 'women\'s rights', 'Brazilian government']
+  }, {
+    name: 'Rubeun',
+    interests: ['edm', 'dark things', 'real estate']
+  }],
+  enroll: (newStudent) => {
+    classroom.students.push(newStudent);
+  }
+};
+
+console.log(typeof classroom.instructor); // what does this log to the console?
+// what happens?
+if(classroom.instructor !== instructors[0]) {
+  console.log("they are not the same thing");
+} else {
+  console.log("they are the same thing");
+}
+
+console.log(classroom.instructor.interests.length); // what about this?
+console.log(classroom.instructor.interests[0]); // what about this?
+console.log(classroom.instructor.interests[classroom.instructor.interests.length - 1]); // this? HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length
+
+
+classroom.instructor.interests.push('javascript'); // what does this do? HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+console.log(classroom.instructor.interests.length); // what is this going to log?
+console.log(classroom.instructor.interests[3]); // what about this?
+
+
+
+console.log(Array.isArray(classroom.students)); // what does this log to the console? HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+console.log(classroom.students[1]); // what does this log to the console?
+console.log(classroom.students[2].interests); // what about this?
+console.log(classroom.students[0].interests[1]); // what about this?
+
+
+
+console.log(classroom.students.length); // what does this log to the console?
+const newStudent = {
+  name: 'Ashley',
+  interests: ['surfing', 'free food', 'lgbt issues']
+};
+
+classroom.enroll(newStudent);
+
+console.log(classroom.students.length); // what does this log to the console? is it different? why?
+
+// what happens?
+if(newStudent.name === classroom.students[3].name) {
+  console.log('they are the same thing');
+} else {
+  console.log('they are not the same thing');
+}
+
+classroom.enroll({
+  name: 'Victor',
+  interests: ['basketball', 'design', 'healthcare']
+});
+
+console.log(classroom.students.length); // what does this log to the console? is it different? why?
+console.log(classroom.students[4].interests[2]); // what is this?
+
+
+const newStudent2 = {
+  name: 'Betsy',
+  interests: ['her son', 'home decor', 'hardware']
+};
+classroom.students.push(newStudent2)
+
+// what happens?
+if(newStudent2.name !== classroom.students[classroom.students.length - 1].name) {
+  console.log('they are not the same thing');
+} else {
+  console.log('they are the same thing');
+}
+
+const elizabethLichtenberg = newStudent2;
+
+// what happens?
+if(newStudent2 === elizabethLichtenberg) {
+  console.log('they are the same thing');
+} else {
+  console.log('they are not the same thing');
+}
+
+let betsyClone = {
+  name: 'Betsy',
+  interests: ['her son', 'home decor', 'hardware']
+};
+
+// what happens?
+if(newStudent2 === betsyClone) {
+  console.log('they are the same thing');
+} else {
+  console.log('they are not the same thing');
+}
+
+
+// what happens?
+betsyClone = elizabethLichtenberg;
+
+if(newStudent2 === betsyClone) {
+  console.log('they are the same thing');
+} else {
+  console.log('they are not the same thing');
+}
